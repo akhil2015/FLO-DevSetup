a82da478744a52d97541951616e7db984a889222
akhil2015 <akhilcool1996@gmail.com>
2018-04-20 23:37:18 +0530

    Removed Duplicate file

--- FLO_0.15.sh
+++ FLO_0.15.sh
@@ -1,15 +1,23 @@
+## WELCOME TO FLO 0.15 INSTALLATION SCRIPT
+
+##  Switch To Home Directory
 cd ~
 
+
+## Installing the Dependencies
 sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
 sudo apt-get install libboost-all-dev
+
+## Installing db4.8 (Only for Ubuntu users)
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install libdb4.8-dev libdb4.8++-dev
 
+## If you want to build FLO-Qt, make sure that the required packages for Qt development are installed. 
 sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
 
-
+## This is the source 
 git clone https://github.com/floblockchain/flo.git
 cd flo
 ./autogen.sh
@@ -17,6 +25,7 @@ cd flo
 make
 make install
 
+## Adding the flo.conf file and creating RPC username and password
 cd ~
 sudo mkdir .flo
 config=".flo/flo.conf"
