b1b04f917d7893d979388350cc17e1e3d59e68b1
akhil2015 <akhilcool1996@gmail.com>
2018-04-22 10:08:50 +0530

    added forced key save with sudo && added date suffix with keys

--- export_wallet.sh
+++ export_wallet.sh
@@ -1,7 +1,7 @@
 ## FLO wallet export key script
 ## flo-qt must be running to use this script
-## Don't use sudo to run this script
 
-target="latest_keys.txt"
-touch $target
+target="keys$(date +%F).txt"
+sudo rm -r $target
+sudo touch $target
 flo-cli dumpwallet $target
